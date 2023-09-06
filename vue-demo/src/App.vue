79d72b05878fa5e93c67fea109f069a255c1b295
3121279430 <104184048+zed11213@users.noreply.github.com>
2023-09-06 23:30:46 +0900

    vue upload

---
+++ vue-demo/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+//import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/demo-icon.jpeg" width="150" height="125" />
+
+    <template>
+      <div id="app">
+        <h1>To-Do List</h1>
+      </div>
+    </template>
+
+  </header>
+
+</template>
+
+}
